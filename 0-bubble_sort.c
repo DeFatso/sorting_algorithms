01ef9df6a73d9541603c2c8fae8158eeb16681b8
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-18 08:16:23 +0000

    bubble sort fourth

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,16 +13,13 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - 1 - i; j++)
 		{
+
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-			}
-			if (array[j] < array[j + 1])
-			{
-				return;
 			}
 		}
 	}
