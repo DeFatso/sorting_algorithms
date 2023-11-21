c192154f3de3b332710626756f6acc91e13e3dea
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-21 07:40:22 +0000

    3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+/**
+ * swap - swaps two values
+ * @a: first value
+ * @b: second value
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	(*a) = (*b);
+	(*b) = temp;
+}
+/**
+ * split - partitions array
+ * @arr: array to partition
+ * @high: highest index
+ * @low: lowest index
+ * Return: returns the partitioning index
+ */
+int split(int arr[], int high, int low)
+{
+	int pivot = arr[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (arr[j] > pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+/**
+ * recursive_quick_sort - sorts both sides of the index
+ * @array: array to be sorted
+ * @size: array size
+ * @high: highest index
+ * @low: lowest index
+ */
+void recursive_quick_sort(int *array, size_t size, int low, int high)
+{
+	if (low < high)
+	{
+		int partition_index = split(array, high, low);
+
+		print_array(array, size);
+
+		recursive_quick_sort(array, size, low, partition_index - 1);
+		recursive_quick_sort(array, size, partition_index + 1, high);
+	}
+}
+/**
+ * quick_sort - sorts array using quick sort
+ * @array: array to be sorted
+ * @size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	recursive_quick_sort(array, size, 0, size - 1);
+}
